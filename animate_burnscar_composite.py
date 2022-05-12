4e15435c708106074f1466aa8824cfa38aba5d1a
Javier <vllgsbr2@llinois.edu>
2022-05-11 22:48:48 -0400

    d

--- animate_burnscar_composite.py
+++ animate_burnscar_composite.py
@@ -6,8 +6,7 @@ import sys
 from datetime import datetime
 import time
 
-
-#python modules I made
+#import python modules I made
 from burn_scar_composites import get_burn_scar_RGB,\
                                  get_normalized_burn_ratio,\
                                  get_BRF_lat_lon,\
@@ -18,30 +17,8 @@ from read_VIIRS_raw_nc_files import get_VJ103_geo,\
                                    get_VJ102_ref,\
                                    get_CLDMSK
 
-
-def regrid(self, data, lat_source, lon_source):
-    '''
-    @param {nd.array} data
-    @param {nd.array} lat_source
-    @param {nd.array} lon_source
-
-    all inputs are the same MxN but data can have depth K
-
-    @return regridded @param {nd.array} data
-    '''
-    from regrid import regrid
-
-    if data.ndim >= 3:
-        data_temp = np.empty(np.shape(data))
-
-        for i in range(data.ndim):
-            data_temp[:,:,i] = regrid(data[:,:,i], self.lat_target,\
-                                     self.lon_target, lat_source, lon_source)
-        return data_temp
-    else:
-        return regrid(data, self.lat_target, self.lon_target, lat_source,\
-                                                        lon_source)
-
+from regrid import regrid_latlon_source2target as regrid_data
+from cloud_water_clear_data import mask_water_and_cloud
 
 #calculate fields to plot
 M_bands_BRF, lat, lon = get_BRF_lat_lon(geo_file, ref_file, which_bands)
@@ -58,62 +35,59 @@ cldmsk, land_water_mask = get_CLDMSK(cldmsk_file)
 burn_scar_composite     = get_burn_scar_composite(R_M7, R_M11)
 
 
-BRF_RGB[np.isnan(BRF_RGB)]                 = -999
 NBR[np.isnan(NBR)]                         = -999
 burn_scar_RGB[np.isnan(burn_scar_RGB)]     = -999
 cldmsk[np.isnan(cldmsk)]                   = -999
 land_water_mask[np.isnan(land_water_mask)] = -999
 burn_scar_composite[np.isnan(burn_scar_composite)] = -999
 
-BRF_RGB         = flip_arr(BRF_RGB)
+
+burn_scar_composite_cloud_water_cleared = mask_water_and_cloud(burn_scar_composite, cldmsk, land_water_mask)
+
+# regrid data #################################################################
+
+# burn_scar_composite = regrid(data, lat_target,lon_target, lat_source, lon_source)
+#need to choose a reference grid using an exemplary granule
+#may 7th 2022 is a good centered granule
+lat_source, lon_source = np.copy(), np.copy()
+
+lat_target, lon_target = np.copy(lat), np.copy(lon)
+# (source_lat, source_lon, target_lat, target_lon, source_data)
+burn_scar_composite_cloud_water_cleared_r = regrid_data(source_lat, source_lon, target_lat, target_lon, burn_scar_composite_cloud_water_cleared)
+cldmsk_r              = regrid_data(source_lat, source_lon, target_lat, target_lon, cldmsk)
+burn_scar_RGB_r       = regrid_data(source_lat, source_lon, target_lat, target_lon, burn_scar_RGB)
+NBR_r                 = regrid_data(source_lat, source_lon, target_lat, target_lon, NBR)
+
+
+
+
+#plot ##########################################################################
+f, ax = plt.subplots(nrows=2, ncols=2, sharex=True, sharey=True, \
+                     figsize=(13,13))
+#flip images over both axis for correct plotting with imshow
 NBR             = flip_arr(NBR)
 burn_scar_RGB   = flip_arr(burn_scar_RGB)
 cldmsk          = flip_arr(cldmsk)
 land_water_mask = flip_arr(land_water_mask)
-burn_scar_composite = flip_arr(burn_scar_composite)
+burn_scar_composite_cloud_water_cleared = flip_arr(burn_scar_composite_cloud_water_cleared)
 
-# group.create_dataset(observables[i], data=data[:,:,i], compression='gzip')
-# group = hf_observables.create_group(time_stamp)
-
-#write data to file
-time_stamp_current = geo_file[-33:-21]
-year     = time_stamp_current[:4]
-DOY      = '{}'.format(time_stamp_current[4:7])
-UTC_hr   = time_stamp_current[8:][:2]
-UTC_min  = time_stamp_current[8:][2:]
-date     = datetime.strptime(year + "-" + DOY, "%Y-%j").strftime("_%m.%d.%Y")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#plot
-f, ax = plt.subplots(nrows=2, ncols=2, sharex=True, sharey=True, \
-                     figsize=(13,13))
-
-im1 = ax[0,0].imshow(1.5*BRF_RGB)
+im1 = ax[0,0].imshow(NBR)
 im2 = ax[0,1].imshow(cldmsk, cmap='Greys')
-im3 = ax[1,0].imshow(land_water_mask, cmap='jet')
+im3 = ax[1,0].imshow(burn_scar_composite_regridded, cmap='jet')
 im4 = ax[1,1].imshow(1.2*burn_scar_RGB)
 
 for a in ax.flat:
     a.set_xticks([])
     a.set_yticks([])
 
-title = 'NOAA-20 VIIRS; {}'.format(ts)
-f.suptitle(ts)
+time_stamp_current = geo_file[-33:-21] #need to replace with filename
+year     = time_stamp_current[:4]
+DOY      = '{}'.format(time_stamp_current[4:7])
+UTC_hr   = time_stamp_current[8:][:2]
+UTC_min  = time_stamp_current[8:][2:]
+date     = datetime.strptime(year + "-" + DOY, "%Y-%j").strftime("_%m.%d.%Y")
+
+title = 'NOAA-20 VIIRS; {}; {:02d}:{:02d} UTC'.format(date, UTC_hr, UTC_min)
+f.suptitle(title)
 plt.tight_layout()
 plt.show()
--- cloud_water_clear_data.py
+++ cloud_water_clear_data.py
@@ -3,17 +3,19 @@
     #     for i in hf_database[time_stamps]:
 
 
-def mask_water_and_cloud(data, cldmsk, land_water_mask):
+def mask_water_and_cloud(data, cldmsk, land_water_mask, clear_water = True, clear_cloud = True):
     '''
     inputs: data, cldmsk, land_water_mask 2D numpy arrays from VIIRS database
     return: masked data
     '''
-    data[cldmsk>=1] = -998
-    water   = 0
-    coastal = 1
-    desert  = 3
-    land    = 4
+    if clear_cloud:
+        data[cldmsk>=1] = -998
+    if clear_water:
+        water   = 0
+        coastal = 1
+        desert  = 3
+        land    = 4
 
-    data[land_water_mask >= coastal] = -997
+        data[land_water_mask >= coastal] = -997
 
     return data
